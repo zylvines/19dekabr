520ee983ec7af7a40ce4e6fcfccf0054fd5e1b51
abuoka <mirajoctane@gmail.com>
2024-12-19 19:13:57 +0500

    shunchaki

---
+++ info git.py
@@ -0,0 +1,7 @@
+''''
+git branch = bor barnchlarni ko'rsatadi
+git branch <branch_name> => create branch
+git checkout <branch_name> => branchga o'tish
+git merge <branch_name> => branchni birlashtiriash
+'''
+
